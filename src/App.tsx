cb863e54b5561ea262c54fe6a25a5360c3df892a
Jorge Saa <piojo.sistematico@gmail.com>
2023-08-28 19:56:18 -0400

    added mobile css

--- src/App.tsx
+++ src/App.tsx
@@ -24,129 +24,124 @@ import twitter from "./assets/images/icon-twitter.svg";
 import pinterest from "./assets/images/icon-pinterest.svg";
 import instagram from "./assets/images/icon-instagram.svg";
 
+import Menu from "./components/Menu";
+
 function App() {
   return (
-    <main>
-      <header>
-        <img src={logo} alt="" />
-        <nav>
-          <ul>
-            <li>
-              <a href="">About</a>
-            </li>
-            <li>
-              <a href="">Careers</a>
-            </li>
-            <li>
-              <a href="">Events</a>
-            </li>
-            <li>
-              <a href="">Products</a>
-            </li>
-            <li>
-              <a href="">Support</a>
-            </li>
-          </ul>
-        </nav>
+    <>
+      <Menu></Menu>
+      <section className="hero-section">
         <h1>Immersive experiences that deliver</h1>
-      </header>
-      <section title="main">
+      </section>
+      <main>
         <div className="leader-section">
           <picture>
             <img src={imgVRMobile} alt="" className="mobile" />
             <img src={imgVRDesktop} alt="" className="desktop" />
           </picture>
-          <h2>The leader in interactive VR</h2>
-          <p>
-            Founded in 2011, Loopstudios has been producing world-class virtual
-            reality projects for some of the best companies around the globe.
-            Our award-winning creations have transformed businesses through
-            digital experiences that bind to their brand.
-          </p>
+          <div className="leader-text">
+            <h2>The leader in interactive VR</h2>
+            <p>
+              Founded in 2011, Loopstudios has been producing world-class
+              virtual reality projects for some of the best companies around the
+              globe. Our award-winning creations have transformed businesses
+              through digital experiences that bind to their brand.
+            </p>
+          </div>
         </div>
         <div className="creation-section">
           <h2>Our creations</h2>
-          <picture>
+          <div className="stacked">
             <img src={imgDEMobile} alt="" className="mobile" />
             <img src={imgDEDesktop} alt="" className="desktop" />
-            <span>Deep earth</span>
-          </picture>
-          <picture>
+            <div className="img-overlay"></div>
+            <span className="vr-title">Deep earth</span>
+          </div>
+          <div className="stacked">
             <img src={imgNAMobile} alt="" className="mobile" />
             <img src={imgNADesktop} alt="" className="desktop" />
-            <span>Night arcade</span>
-          </picture>
-          <picture>
+            <div className="img-overlay"></div>
+            <span className="vr-title">Night arcade</span>
+          </div>
+          <div className="stacked">
             <img src={imgSTMobile} alt="" className="mobile" />
             <img src={imgSTDesktop} alt="" className="desktop" />
-            <span>Soccer team VR</span>
-          </picture>
-          <picture>
+            <div className="img-overlay"></div>
+            <span className="vr-title">Soccer team VR</span>
+          </div>
+          <div className="stacked">
             <img src={imgTGMobile} alt="" className="mobile" />
             <img src={imgTGDesktop} alt="" className="desktop" />
-            <span>The grid</span>
-          </picture>
-          <picture>
+            <div className="img-overlay"></div>
+            <span className="vr-title">The grid</span>
+          </div>
+          <div className="stacked">
             <img src={imgFAMobile} alt="" className="mobile" />
             <img src={imgFADesktop} alt="" className="desktop" />
-            <span>From up above VR</span>
-          </picture>
-          <picture>
+            <div className="img-overlay"></div>
+            <span className="vr-title">From up above VR</span>
+          </div>
+          <div className="stacked">
             <img src={imgPBMobile} alt="" className="mobile" />
             <img src={imgPBDesktop} alt="" className="desktop" />
-            <span>Pocket borealis</span>
-          </picture>
-          <picture>
+            <div className="img-overlay"></div>
+            <span className="vr-title">Pocket borealis</span>
+          </div>
+          <div className="stacked">
             <img src={imgTCMobile} alt="" className="mobile" />
             <img src={imgTCDesktop} alt="" className="desktop" />
-            <span>The curiosity</span>
-          </picture>
-          <picture>
+            <div className="img-overlay"></div>
+            <span className="vr-title">The curiosity</span>
+          </div>
+          <div className="stacked">
             <img src={imgMFMobile} alt="" className="mobile" />
             <img src={imgMFDesktop} alt="" className="desktop" />
-            <span>Make it fisheye</span>
-          </picture>
+            <div className="img-overlay"></div>
+            <span className="vr-title">Make it fisheye</span>
+          </div>
           <button>See all</button>
         </div>
-      </section>
+      </main>
       <footer>
         <img src={logo} alt="" />
-        <nav>
+        <nav className="footer-nav">
           <ul>
             <li>
-              <a href="">About</a>
+              <a href="#">About</a>
             </li>
             <li>
-              <a href="">Careers</a>
+              <a href="#">Careers</a>
             </li>
             <li>
-              <a href="">Events</a>
+              <a href="#">Events</a>
             </li>
             <li>
-              <a href="">Products</a>
+              <a href="#">Products</a>
             </li>
             <li>
-              <a href="">Support</a>
+              <a href="#">Support</a>
             </li>
           </ul>
         </nav>
         <div className="social-links">
-          <a href="">
+          <a href="#">
             <img src={facebook} alt="" />
           </a>
-          <a href="">
+          <a href="#">
             <img src={twitter} alt="" />
           </a>
-          <a href="">
+          <a href="#">
             <img src={pinterest} alt="" />
           </a>
-          <a href="">
+          <a href="#">
             <img src={instagram} alt="" />
           </a>
         </div>
-        <span>© 2021 Loopstudios. All rights reserved.</span>
+        <span className="text-rights">
+          © 2021 Loopstudios. All rights reserved.
+        </span>
       </footer>
-    </main>
+    </>
   );
 }
 
